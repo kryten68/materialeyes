336f414eb1bfc6ad18551073fcc8855d23951f3c
stuarthenderson <kryten68@googlemail.com>
2020-02-10 19:37:40 +0000

    Initial commit.

---
+++ app_assets/components/js/buttons.js
@@ -0,0 +1,10 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=->
+// Initialise the collapsible and expandable areas:
+let elem = document.querySelector('.collapsible.expandable');
+let instance = M.Collapsible.init(elem, {
+    accordion: false
+});
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=->
+
+
+
---
+++ main.js
@@ -0,0 +1,56 @@
+const electron      = require('electron');
+const app           = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const ipcMain       = electron.ipcMain;
+const path          = require('path');
+const url           = require('url');
+
+
+
+let win;
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow);
+
+function createWindow () {
+    // Create the browser window.
+    win = new BrowserWindow({
+        width: 1600,
+        height: 800,
+        frame: true,
+        webPreferences: {
+            //webSecurity: false,
+            nodeIntegration: true
+        }
+    })
+
+
+    // and load the main.html of the app.
+    win.loadFile('main.html');
+
+    // Open the DevTools.
+    win.webContents.openDevTools()
+}
+
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
